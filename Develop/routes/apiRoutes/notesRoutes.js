199addf093416d85352f94db34b0c1dab05de319
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-01-22 15:43:04 -0800

    button path fixed

--- Develop/routes/apiRoutes/notesRoutes.js
+++ Develop/routes/apiRoutes/notesRoutes.js
@@ -4,9 +4,9 @@ const { notes } = require('../../db/db');
 
 router.get('/notes', (req, res) => {
   let results = notes;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
+  // if (req.query) {
+  //   results = filterByQuery(req.query, results);
+  // }
   res.json(results);
 });
 
