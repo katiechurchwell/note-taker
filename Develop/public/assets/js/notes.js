6086a50cda0537828b7e5b3566f714aba356f994
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-01-22 23:33:18 -0800

    save button toggle working

--- Develop/public/assets/js/notes.js
+++ Develop/public/assets/js/notes.js
@@ -1,6 +1,13 @@
 const $noteForm = document.querySelector("#form");
 const $displayArea = document.querySelector("#display-area");
 
+noteTitle = document.querySelector(".note-title");
+noteText = document.querySelector(".note-textarea");
+saveNoteBtn = document.querySelector(".save-note");
+newNoteBtn = document.querySelector(".new-note");
+noteList = document.querySelectorAll(".list-container .list-group");
+
+//generate note list
 const printResults = (resultArr) => {
   const noteHTML = resultArr.map(({ title, text }) => {
     return `
@@ -16,6 +23,7 @@ const printResults = (resultArr) => {
   $displayArea.innerHTML = noteHTML.join("");
 };
 
+//retrieve note database
 const fetchNotes = (formData = {}) => {
   let queryUrl = "/api/notes";
 
@@ -31,12 +39,85 @@ const fetchNotes = (formData = {}) => {
     });
 };
 
+//show/hide save button
+const show = (elem) => {
+  elem.style.display = "inline";
+};
+
+const hide = (elem) => {
+  elem.style.display = "none";
+};
+
+//not running
+const handleRenderSaveBtn = () => {
+  if (noteTitle.value != "") {
+    show(saveNoteBtn);
+  } else {
+    hide(saveNoteBtn);
+  }
+};
+
+//saving notes
 const handleGetNotesSubmit = (event) => {
   event.preventDefault();
   const noteObject = { id, title };
   fetchNotes(noteObject);
 };
 
-$noteForm.addEventListener("submit", handleGetNotesSubmit);
+//NOTE VIEW STUFF??
+const renderActiveNote = () => {
+  hide(saveNoteBtn);
 
+  if (activeNote.id) {
+    noteTitle.setAttribute("readonly", true);
+    noteText.setAttribute("readonly", true);
+    noteTitle.value = activeNote.title;
+    noteText.value = activeNote.text;
+  } else {
+    noteTitle.removeAttribute("readonly");
+    noteText.removeAttribute("readonly");
+    noteTitle.value = "";
+    noteText.value = "";
+  }
+};
+
+//sets the activeNote and displays it?
+const handleNoteView = (e) => {
+  e.preventDefault();
+  activeNote = JSON.parse(e.target.parentElement.getAttribute("data-note"));
+  renderActiveNote();
+};
+
+// Sets the activeNote to and empty object and allows the user to enter a new note
+const handleNewNoteView = (e) => {
+  activeNote = {};
+  renderActiveNote();
+};
+
+//event listeners
+saveNoteBtn.addEventListener("submit", handleGetNotesSubmit);
+// saveNoteBtn.addEventListener("click", handleNoteSave); //write function
+newNoteBtn.addEventListener("click", handleNewNoteView);
+noteTitle.addEventListener("input", handleRenderSaveBtn); //not working
+noteText.addEventListener("input", handleRenderSaveBtn); //working
+
+//deleting notes
+// const handleNoteDelete = (e) => {
+//   // Prevents the click listener for the list from being called when the button inside of it is clicked
+//   e.stopPropagation();
+
+//   const note = e.target;
+//   const noteId = JSON.parse(note.parentElement.getAttribute("data-note")).id;
+
+//   if (activeNote.id === noteId) {
+//     activeNote = {};
+//   }
+
+//   deleteNote(noteId).then(() => {
+//     // getAndRenderNotes();
+//     renderActiveNote();
+//   });
+// };
+
+//generate note list
 fetchNotes();
